adab538e74935f67c2a3aa139acfd46fded2e26c
Jus4 <leskinen.jussi@gmail.com>
2025-08-11 12:33:10 +0300

    feat: tmux config to separate file

---
+++ pkgs/tmux/default.nix
@@ -0,0 +1,28 @@
+{ pkgs, ... }:
+
+{
+  # Tmux
+  enable = true;
+  terminal = "tmux-256color";
+  historyLimit = 100000;
+  # enableVim = true;
+  # tmuxPlugins.powerline.enable = true;
+  plugins = with pkgs;
+    [
+      tmuxPlugins.better-mouse-mode
+      tmuxPlugins.power-theme
+      tmuxPlugins.prefix-highlight
+      tmuxPlugins.sensible
+      tmuxPlugins.yank
+    ];
+  extraConfig = ''
+    set -g @plugin 'erikw/tmux-powerline'
+    bind r source-file ~/.config/tmux/tmux.conf \; display "Reloaded!"
+    set-option -g status-position top 
+    unbind C-s
+    set -g prefix C-s
+    bind C-s send-prefix
+  '';
+}
+
+
